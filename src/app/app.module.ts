1d99d7e9c1118b072bb05bb040bc46aacfeac9f0
ersimont <8042088+ersimont@users.noreply.github.com>
2019-09-21 10:26:11 -0400

    use expansion panels in options

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,24 +1,24 @@
-import { BrowserModule } from "@angular/platform-browser";
-import { NgModule } from "@angular/core";
-import { AppComponent } from "./app.component";
-import { ServiceWorkerModule } from "@angular/service-worker";
-import { environment } from "../environments/environment";
 import { HttpClientModule } from "@angular/common/http";
-import { ChartsModule } from "ng2-charts";
-import { GraphComponent } from "./graph/graph.component";
+import { NgModule } from "@angular/core";
 import { FormsModule } from "@angular/forms";
-import { StoreModule } from "@ngrx/store";
-import { NasModelModule, ngAppStateReducer } from "ng-app-state";
-import { StoreDevtoolsModule } from "@ngrx/store-devtools";
-import { BrowserAnimationsModule } from "@angular/platform-browser/animations";
 import {
   MatButtonModule,
+  MatExpansionModule,
   MatIconModule,
-  MatListModule,
   MatSidenavModule,
   MatSlideToggleModule,
   MatToolbarModule,
 } from "@angular/material";
+import { BrowserModule } from "@angular/platform-browser";
+import { BrowserAnimationsModule } from "@angular/platform-browser/animations";
+import { ServiceWorkerModule } from "@angular/service-worker";
+import { StoreModule } from "@ngrx/store";
+import { StoreDevtoolsModule } from "@ngrx/store-devtools";
+import { NasModelModule, ngAppStateReducer } from "ng-app-state";
+import { ChartsModule } from "ng2-charts";
+import { environment } from "../environments/environment";
+import { AppComponent } from "./app.component";
+import { GraphComponent } from "./graph/graph.component";
 import { OptionsComponent } from "./options/options.component";
 
 @NgModule({
@@ -30,8 +30,8 @@ import { OptionsComponent } from "./options/options.component";
     FormsModule,
     HttpClientModule,
     MatButtonModule,
+    MatExpansionModule,
     MatIconModule,
-    MatListModule,
     MatSidenavModule,
     MatSlideToggleModule,
     MatToolbarModule,
