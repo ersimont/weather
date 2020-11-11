ef866a49d6529fd37ed85f9a17ce2020a5d09225
ersimont <8042088+ersimont@users.noreply.github.com>
2020-11-11 09:56:05 -0500

    update s-libs to 0.2

--- src/app/to-replace/event-tracking/event-tracking.service.ts
+++ src/app/to-replace/event-tracking/event-tracking.service.ts
@@ -2,6 +2,8 @@ import { Injectable } from '@angular/core';
 import { EventTrackingConfig } from 'app/to-replace/event-tracking/event-tracking-config';
 
 // This was previously written to use the newer library "gtag.js" instead of the older "analyticsjs", but there was no official otp-out of cookies in the newer version. People on the internet report being able to configure it with the setting `client_storage: none`, but that was undocumented. It loaded `analytics.js` behind the scenes anyway, so I reverted to that "old" library.
+//
+// It looks like support for that is coming with the "consent" feature, currently in beta: https://developers.google.com/gtagjs/devguide/consent. At that time it may make sense to use the firebase analytics features instead of this self-created package.
 
 @Injectable({ providedIn: 'root' })
 export class EventTrackingService {
