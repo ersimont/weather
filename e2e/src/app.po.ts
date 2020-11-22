e81f7866d407821eb53a4b0da4f6ec3e1df0f134
ersimont <8042088+ersimont@users.noreply.github.com>
2020-11-21 19:30:58 -0500

    manually apply remaining diff from angular upgrade

--- e2e/src/app.po.ts
+++ e2e/src/app.po.ts
@@ -1,13 +1,11 @@
 import { browser, by, element } from 'protractor';
 
 export class AppPage {
-  navigateTo(): Promise<any> {
-    return browser.get(browser.baseUrl) as Promise<any>;
+  async navigateTo(): Promise<any> {
+    return browser.get(browser.baseUrl);
   }
 
-  getTitleText(): Promise<string> {
-    return element(by.css('app-root .content span')).getText() as Promise<
-      string
-    >;
+  async getTitleText(): Promise<string> {
+    return element(by.css('app-root .content span')).getText();
   }
 }
