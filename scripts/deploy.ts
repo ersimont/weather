842d1a18f30eb1a85c3593b015eb5ee5633a4d5f
Eric Simonton <ersimont@gmail.com>
2026-07-18 08:02:59 -0400

    update deploy script

--- scripts/deploy.ts
+++ scripts/deploy.ts
@@ -1,67 +1,64 @@
-import { browser } from '@bugsnag/source-maps';
-import { execSync } from 'child_process';
+import { assert } from '@s-libs/js-core';
 import { program } from 'commander';
-import * as fs from 'fs';
-import { join } from 'path';
+import { $ } from 'zx';
 
 const sourceDir = 'dist/weather';
 const bugsnagApiKey = 'ed6690791c812d163fb92d4ad7a21ef4';
+
+$.shell = 'powershell.exe';
+$.prefix = '';
 
 program
   .command('production')
   .description('deploy to production')
   .action(async () => {
-    compileProduction();
-    await uploadSourcemaps();
-    deployProduction();
+    await compileProduction();
+    await deployProduction();
+    // await uploadSourcemaps('https://weather.simonton.app');
   });
 
 program
   .command('staging [channelId]')
   .description('deploy to staging')
   .action(async (channelId = 'staging') => {
-    compileStaging();
-    await uploadSourcemaps();
-    deployStaging(channelId);
+    await compileStaging();
+    const baseUrl = await deployStaging(channelId);
+    // await uploadSourcemaps(baseUrl);
   });
 
-program.parseAsync(process.argv);
+await program.parseAsync();
 
-function compileStaging(): void {
+async function compileStaging(): Promise<void> {
   console.log('compiling staging code...');
-  execSync('ng build --configuration=staging', { stdio: 'inherit' });
+  await $`ng build --configuration=staging`;
 }
 
-function compileProduction(): void {
+async function compileProduction(): Promise<void> {
   console.log('compiling production code...');
-  execSync('ng build', { stdio: 'inherit' });
+  await $`ng build`;
 }
 
-async function uploadSourcemaps(): Promise<void> {
-  const sourceFileNames = fs
-    .readdirSync(sourceDir)
-    .filter((fileName) => fileName.match(/.*.map/));
-  for (const sourceFileName of sourceFileNames) {
-    console.log('uploading', sourceFileName);
-    const sourceMap = join(sourceDir, sourceFileName);
-    const bundleUrl = '*/' + sourceFileName.slice(0, -4);
-    await browser.uploadOne({
-      apiKey: bugsnagApiKey,
-      bundleUrl,
-      sourceMap,
-      overwrite: true,
-    });
-  }
-}
-
-function deployStaging(channelId: string): void {
+async function deployStaging(channelId: string): Promise<string> {
   console.log(`deploying to preview channel ${channelId}...`);
-  execSync(`firebase hosting:channel:deploy ${channelId}`, {
-    stdio: 'inherit',
-  });
+  const { stdout } = await $`firebase hosting:channel:deploy ${channelId}`.pipe(
+    process.stdout,
+  );
+  const matches = stdout.match(
+    /https:\/\/simonton-weather--staging-\S+\.web\.app/u,
+  );
+  assert(matches);
+  return matches[0];
 }
 
-function deployProduction(): void {
+async function deployProduction(): Promise<void> {
   console.log('deploying to production...');
-  execSync('firebase deploy --only hosting', { stdio: 'inherit' });
+  await $`firebase deploy --only hosting`;
 }
+
+// TODO: the bugsnag CLI is not installing properly. Try again later.
+// async function uploadSourcemaps(baseUrl: string): Promise<void> {
+//   console.log('uploading sourcemaps...');
+//   await $({
+//     cwd: sourceDir,
+//   })`npx bugsnag-cli upload js --api-key=${bugsnagApiKey} --baseUrl=${baseUrl}`;
+// }
